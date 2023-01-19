bd4e858842b3aa69d510b65e8021f90acd3ac358
kkwondev <kkwoncokr@gmail.com>
2023-01-20 00:15:45 +0900

    list css add

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,73 @@
 <template>
-  <div>
-    <p>홈페이지 입니다.</p>
+  <div class="app">
+    <main>
+      <div>
+        <input type="text" />
+      </div>
+      <ul>
+        <li class="flex item" v-for="product in products" :key="product.id">
+          <img
+            class="product-image"
+            :src="product.imageUrl"
+            :alt="product.name"
+          />
+          <p>{{ product.name }}</p>
+          <span>{{ product.price }}</span>
+        </li>
+      </ul>
+    </main>
   </div>
 </template>
-
 <script>
+import axios from 'axios'
+// import ProductList from '~/components/ProductList.vue'
 export default {
-  name: 'IndexPage',
+  // components: { ProductList },
+  async asyncData() {
+    const response = await axios.get('http://localhost:3000/products')
+    const products = response.data.map((item) => {
+      return {
+        ...item,
+        imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+      }
+    })
+    return { products }
+  },
+  // data() {},
+  // async created() {},
 }
 </script>
+
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
+.item {
+  display: inline-block;
+  width: 400px;
+  height: 300px;
+  text-align: center;
+  margin: 0 0.5rem;
+  cursor: pointer;
+}
+.product-image {
+  width: 400px;
+  height: 250px;
+}
+.app {
+  position: relative;
+}
+.cart-wrapper {
+  position: sticky;
+  float: right;
+  bottom: 50px;
+  right: 50px;
+}
+.cart-wrapper .btn {
+  display: inline-block;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 500;
+}
+</style>
